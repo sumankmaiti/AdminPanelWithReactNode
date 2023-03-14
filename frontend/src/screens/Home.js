83a15b55b687bf7f383a3b6575caf31eda6a8a95
sumankmaiti <sumankmaiti01@gmail.com>
2023-03-14 17:52:11 +0530

    home-screen modification

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
 import './Home.css'
 
 const Home = () => {
@@ -9,28 +8,45 @@ const Home = () => {
 	return(
 		<div>
 			<h2> Employee Management </h2>
-			<div>
-				<input 
-				type='text'
-				name='loginId' 
-				id='loginId' 
-				value={user} 
-				onChange={(e) => setUser(e.target.value)} 
-				placeholder='phone number or email'/>
-			</div>
-			<div>
-				<input
-				type='password'
-				name='password'
-				id='password'
-				value={password}
-				placeholder='Password'
-				onChange={(e) => setPassword(e.target.value)} />
-			</div>	
-			<div>
-				<button type='submit' id='submit' > Login </button>
-				<button type='button' id='reset'> Reset </button>
-				<Link to='/register'> I am a NEW HERE </Link>
+			<div className='container' id='container'>
+				<div className='form-container sign-up-container'>
+					<form action='#'>
+						<h1> Create Account </h1>
+						Registration inputs
+					</form>
+				</div>
+
+				<div className='form-container sign-in-container'>
+					<form action='#'>
+						<h1> Sign in </h1>
+						<input 
+						type='text'
+						name='loginId' 
+						id='loginId' 
+						value={user} 
+						onChange={(e) => setUser(e.target.value)} 
+						placeholder='phone number or email'/>
+
+						<input
+						type='password'
+						name='password'
+						id='password'
+						value={password}
+						placeholder='Password'
+						onChange={(e) => setPassword(e.target.value)} />
+
+						<button type='submit' id='submit' > Login </button>
+					</form>
+				</div>
+				<div className='overlay-container'>
+					<div className='overlay'>
+						<div className='overlay-panel overlay-left'>
+							<h1> Welcome Back! </h1>
+							<p> </p>
+						</div>
+					</div>
+					
+				</div>
 			</div>
 		</div>
 	)
